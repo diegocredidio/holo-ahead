f90a7c9a211e95a71d419e69226345d94f28673c
Diego Credidio <diegocredidio@gmail.com>
2018-07-05 16:54:20 -0300

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar is-black is-fixed-top" role="navigation" aria-label="main navigation">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    Home
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <!-- navbar start, navbar end -->
+            </div>
+        </div>
+    </nav>
+</template>
