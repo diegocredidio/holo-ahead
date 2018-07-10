43bbae627d73ab2d59e80645429fb4ae94460241
Diego Credidio <diegocredidio@gmail.com>
2018-07-10 14:36:00 -0300

    Metadata keywords updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,21 @@ module.exports = {
     title: 'holo-ahead',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'keywords',
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'keywords',
+        name: 'keywords',
+        content:
+          'holo, ahead, 3d, holografia, mobilidade, portátil, propaganda, campanha'
+      },
       {
         hid: 'description',
         name: 'description',
-        content: 'holo ahead - a vida se passa na rua'
+        content: 'Holo Ahead - A vida se passa na rua'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -22,16 +32,17 @@ module.exports = {
     { src: '~/assets/css/main.scss', lang: 'scss' },
     { src: 'font-awesome/scss/font-awesome.scss', lang: 'scss' }
   ],
+
+  plugins: [{ src: '~plugins/ga.js', ssr: false }],
+
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+
   /*
   ** Build configuration
   */
- plugins: [
-  { src: '~plugins/ga.js', ssr: false }
-],
   build: {
     /*
     ** Run ESLint on save
