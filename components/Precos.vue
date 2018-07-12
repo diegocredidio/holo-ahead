68cf16cb7774fd8e71841b7e9c17408c36dda6c9
Diego Credidio <diegocredidio@gmail.com>
2018-07-12 15:03:11 -0300

    Contact form

--- components/Precos.vue
+++ components/Precos.vue
@@ -51,6 +51,90 @@
                     </div>
                 </div>
             </div>
+            <div class="container">
+                <!-- Begin MailChimp Signup Form -->
+                <div id="mc_embed_signup">
+                    <div class="form_msg">
+                        Quer Holo Ahead para a sua campanha? Preencha os campos abaixo que entramos em contato com você em até 30 minutos.
+                    </div>
+                    <form action="https://dlion.us17.list-manage.com/subscribe/post?u=a7bcd30029bcafc61445a9536&amp;id=d44bb0aa63" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                        <div id="mc_embed_signup_scroll">
+
+                            <div class="mc-field-group">
+                                <label for="mce-EMAIL">Email </label>
+                                <div class="label-down">
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                </div>
+                            </div>
+                            <div class="mc-field-group">
+                                <label for="mce-NAME">Nome </label>
+                                <div class="label-down">
+                                    <input type="text" value="" name="NAME" class="" id="mce-NAME">
+                                </div>
+                            </div>
+                            <div class="mc-field-group size1of2">
+                                <label for="mce-PHONE">Telefone/Whatsapp </label>
+                                <div class="label-down">
+                                    <input type="text" name="PHONE" class="" value="" id="mce-PHONE">
+                                </div>
+                            </div>
+                            <div class="mc-field-group input-group row">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <strong>Como você prefere que a gente entre em Contato? </strong>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <ul>
+                                                <li><input type="radio" value="Whatsapp" name="MMERGE5" id="mce-MMERGE5-0" checked="checked">
+                                                    <label for="mce-MMERGE5-0">Whatsapp</label>
+                                                </li>
+                                                <li><input type="radio" value="Email" name="MMERGE5" id="mce-MMERGE5-1">
+                                                    <label for="mce-MMERGE5-1">Email</label>
+                                                </li>
+                                                <li><input type="radio" value="Telefone" name="MMERGE5" id="mce-MMERGE5-2">
+                                                    <label for="mce-MMERGE5-2">Telefone</label>
+
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a7bcd30029bcafc61445a9536_d44bb0aa63" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </div>
+                    </form>
+                </div>
+                <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+                <script type='text/javascript'>
+                (function($) {
+                    window.fnames = new Array();
+                    window.ftypes = new Array();
+                    fnames[0] = 'EMAIL';
+                    ftypes[0] = 'email';
+                    fnames[1] = 'NAME';
+                    ftypes[1] = 'text';
+                    fnames[2] = 'LNAME';
+                    ftypes[2] = 'text';
+                    fnames[3] = 'ADDRESS';
+                    ftypes[3] = 'address';
+                    fnames[4] = 'PHONE';
+                    ftypes[4] = 'phone';
+                    fnames[5] = 'MMERGE5';
+                    ftypes[5] = 'radio';
+                }(jQuery));
+                var $mcj = jQuery.noConflict(true);
+                </script>
+                <!--End mc_embed_signup-->
+            </div>
         </section>
         <section class="precos-top">
             <div class="container">
