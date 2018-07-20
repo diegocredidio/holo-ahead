d56550a9a4e90c6d49d8e752e13e4cea81d59664
Diego Credidio <diegocredidio@gmail.com>
2018-07-20 17:24:00 -0300

    All Changes

--- components/Mesmice.vue
+++ components/Mesmice.vue
@@ -15,6 +15,35 @@
             </section>
         </section>
 
+        <section class="hero-news" id="mesmice2-link" style="padding-bottom: 0px;">
+            <section class="container">
+                <div class="d-flex flex-row">
+                    <div class="">
+                        <div class="holo-title-h1 underlined">
+                            <div class="title-text">NEWS</div>
+                        </div>
+                    </div>
+                    <div class="pl-5">
+                        <a class="nav-link" href="https://medium.com/holo-ahead">
+                            <img class="leia-mais" src="~/assets/images/leia_mais.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5">
+                        <img class="news-headline" src="~/assets/images/news1.png" alt="">
+                    </div>
+                    <div class="col-lg">
+                        <img class="news-headline" src="~/assets/images/news2.png" alt="">
+                        <div class="news-globe d-sm-none d-md-block">
+                            <img src="~/assets/images/news-globe.png" alt="">
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+        </section>
+
         <!-- COMO FUNCIONA -->
         <section class="hero-white" id="comofunciona-link" style="padding-bottom: 0px;">
             <section class="container">
